20f9a86f313d0076bd1b4846c8efe9b4bae88a40
JunCodera1 <minhtien0601200@gmail.com>
2024-09-27 12:27:52 +0700

    First Commit

---
+++ src/main/java/test/spiring/spring/SpringBinaApplication.java
@@ -0,0 +1,13 @@
+package test.spiring.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBinaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBinaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/test/spiring/spring/SpringBinaApplicationTests.java
@@ -0,0 +1,13 @@
+package test.spiring.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBinaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
